e249567aae43fcd83dd1b07f039e3bb8b7527a6b
hyunmyeong <tknfk@naver.com>
2022-11-17 15:20:37 +0900

    setting

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>안전한거 맞겠지....?</Text>
       <StatusBar style="auto" />
     </View>
   );
